3c3f663ad2f678c6df901ac8527d21202c2374c6
raphaelkapcan <raphael.kaplan@lhsla.org>
2026-08-31 09:02:23 -0700

    assignment 1a

---
+++ assignment 1a.py
@@ -0,0 +1,15 @@
+# Kaplan, Raphael
+# Computer Programming, Period 4
+# Assignemnt: Homework 1
+# August 27, 2026
+print("Hello World!")
+print("Hello my name is Raphael!")
+print("I love python!")
+print("16")
+print(5+5)
+print(4*3)
+print(50-40)
+print(100/5)
+print(100//5) # This rounds to the nearest whole number
+print(2**4)
+print(6*3-4+8//2)
---
+++ assignment 1b.py
@@ -0,0 +1,12 @@
+# Kaplan, Raphael
+# Computer Programming, Period 4
+# Assignment: Homework 1b
+# August 31,2026
+
+fav_hobby = "Golf"
+name = "Raphael"
+print("Hello " + name + ", would you like to learn some python today?")
+print(name.lower())
+print(name.upper())
+print(name.title())
+print()
