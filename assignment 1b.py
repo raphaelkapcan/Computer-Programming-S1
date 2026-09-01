f9f318ec25f8ad783304a95909bc29d8bdf4f102
raphaelkapcan <raphael.kaplan@lhsla.org>
2026-09-01 12:45:23 -0700

    completeled assignment 1b

--- assignment 1b.py
+++ assignment 1b.py
@@ -5,8 +5,24 @@
 
 fav_hobby = "Golf"
 name = "Raphael"
-print("Hello " + name + ", would you like to learn some python today?")
+famous_person = "Theodore Roosevelt"
+whitespacename = " Raphael "
+filename = "python_notes.txt"
+favourite_number = 2
+
+print("Hello " ,name, ", would you like to learn some python today?")
 print(name.lower())
 print(name.upper())
 print(name.title())
-print()
+print("Theodore Roosevelt once said, \"Believe you can and you're halfway there.\"")
+print( "",famous_person, "once said, \"Believe you can and you're halfway there.\"")
+print(whitespacename)
+print(whitespacename.lstrip())
+print(whitespacename.rstrip())
+print(whitespacename.strip())
+print(filename.removesuffix(".txt"))
+print(8+3)
+print(12-1)
+print(11*1)
+print(22//2)
+print(favourite_number)
